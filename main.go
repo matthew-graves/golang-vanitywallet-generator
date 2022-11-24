ce2c29c9a7cf5335cd00a3e4bd598bb280e38b50
Matthew Graves <matthew.graves@ironthornsecurity.com>
2022-11-23 18:51:43 -0800

    Minor performance increase & error on unusable characters in wallet address

--- main.go
+++ main.go
@@ -14,18 +14,19 @@ import (
 
 func GenWallet() {
 	account := crypto.GenerateAccount()
-	if strings.HasPrefix(account.Address.String(), strings.ToUpper(os.Args[1])) {
-		fmt.Println("\n----------------------------------")
-		fmt.Println("Wallet Found!")
-		fmt.Println(account.Address.String())
-		mnemonic, err := mnemonic.FromPrivateKey(account.PrivateKey)
-		if err != nil {
-			return
+	if account.Address.String()[0] == os.Args[1][0] {
+		if strings.HasPrefix(account.Address.String(), strings.ToUpper(os.Args[1])) {
+			fmt.Println("\n----------------------------------")
+			fmt.Println("Wallet Found!")
+			fmt.Println(account.Address.String())
+			mnemonic, err := mnemonic.FromPrivateKey(account.PrivateKey)
+			if err != nil {
+				return
+			}
+			fmt.Println(mnemonic)
+			os.Exit(0)
 		}
-		fmt.Println(mnemonic)
-		os.Exit(0)
 	}
-
 }
 
 func main() {
@@ -35,7 +36,11 @@ func main() {
 		fmt.Println("No parameters provided, please add a word or phrase to search for.")
 		fmt.Println("Example Usage: ./vanitywalletgenerator.exe LIBERTY")
 		fmt.Println()
-		os.Exit(0)
+		os.Exit(1)
+	}
+	if strings.ContainsAny(os.Args[1], "0189") {
+		fmt.Println("Invalid Input, Characters 0, 1, 8, and 9 are not supported in Algorand Wallets")
+		os.Exit(1)
 	}
 	fmt.Printf("Start Time: %s\n", time.Now())
 	x := 1
